dd90fba00d73d614502f3765668fb4f2d3c81b0a
Patricia Magalhães <paath.magalhaes2@gmail.com>
2023-10-10 18:50:49 -0300

    fix: atualização nome de rota

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,17 @@
-const express = require("express")
+const express = require("express");
 
-const { registerUser } = require("./controllers/controllersUsers")
-const validateRequest = require("./middlewares/validateRequest")
-const userSchema = require("./validations/userSchema")
+const { registerUser } = require("./controllers/controllersUsers");
+const validateRequest = require("./middlewares/validateRequest");
+const userSchema = require("./validations/userSchema");
 
 const { getCategories } = require("./controllers/categoryController");
 
-const route = express()
+const route = express();
 
-route.post('/usuario', validateRequest(userSchema), registerUser)
+route.post("/usuario", validateRequest(userSchema), registerUser);
 
 route.get("/", (req, res) => res.json("hello, world"));
 
-route.get("/categorias", getCategories);
-
+route.get("/categoria", getCategories);
 
 module.exports = route;
-
