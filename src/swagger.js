8669cfe8e1a2746016ff4944252bfb57f6f36ffd
Patricia Magalhães <paath.magalhaes2@gmail.com>
2023-10-23 22:40:32 -0300

    doc: criada documentação no swagger

---
+++ src/swagger.js
@@ -0,0 +1,21 @@
+const swaggerAutogen = require("swagger-autogen");
+
+const doc = {
+  info: {
+    version: "1.0.0",
+    title: "Atena PDV",
+    description:
+      "ASistema de PDV desenvolvido como projeto de conclusão do curso de Desenvolvimento de Software com foco em Backend da Cubos Academy.",
+  },
+  host: "localhost:3000",
+  basePath: "/",
+  schemes: ["http", "https"],
+  consumes: ["application/json"],
+  produces: ["application/json"],
+  tags: [],
+};
+
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./routes.js"];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
