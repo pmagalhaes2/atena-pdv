eda57969bbf3a3af706a72b5acc7375d69762e65
Patricia Magalhães <paath.magalhaes2@gmail.com>
2023-10-25 20:45:59 -0300

    refact: ajuste cadastro de pedido

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,7 +1,6 @@
 const knex = require("../connections/postgres");
 const sendEmail = require("../connections/nodemailer");
 const htmlCompiler = require("../utils/htmlCompiler");
-const { exist } = require("joi");
 
 const registerOrder = async (req, res) => {
   const { cliente_id, observacao, pedido_produtos } = req.body;
@@ -15,6 +14,8 @@ const registerOrder = async (req, res) => {
       return res.status(404).json({ mensagem: "Cliente não encontrado." });
     }
 
+    let valor_total = 0;
+
     for (const order of pedido_produtos) {
       const existingProduct = await knex("produtos")
         .where({ id: order.produto_id })
@@ -25,64 +26,59 @@ const registerOrder = async (req, res) => {
           mensagem: `Nenhum produto com o id ${order.produto_id} foi encontrado.`,
         });
       }
+
+      const productPrice = await knex("produtos")
+        .where({ id: order.produto_id })
+        .select("valor as valor_produto")
+        .first();
+
+      valor_total += order.quantidade_produto * productPrice.valor_produto;
     }
 
-    // TODO - Criar lógica para cadastrar os pedidos somente se todos contiverem o campo produto_id válido
-
-    const newOrder = await knex('pedidos')
+    const newOrder = await knex("pedidos")
       .insert({
         cliente_id,
-        observacao
+        observacao,
+        valor_total,
       })
-      .returning('*');
-
-    let valor_total = 0;
+      .returning("*");
 
     for (const order of pedido_produtos) {
-      const productPrice = await knex('produtos')
-        .where({ id: order.produto_id })
-        .select('valor as valor_produto');
-
-      const productOrder = await knex('pedido_produtos')
+      await knex("pedido_produtos")
         .insert({
           pedido_id: newOrder[0].id,
           produto_id: order.produto_id,
           quantidade_produto: order.quantidade_produto,
-          valor_produto: productPrice[0].valor_produto
+          valor_produto: knex.raw("(SELECT valor FROM produtos WHERE id = ?)", [
+            order.produto_id,
+          ]),
         })
-        .returning('*')
-
-      valor_total += (order.quantidade_produto * productPrice[0].valor_produto);
+        .returning("*");
     }
-
-    await knex('pedidos')
-      .where({ id: newOrder[0].id })
-      .update({ valor_total });
 
     const orderObject = {
       numero_do_pedido: newOrder[0].id,
       produtos: pedido_produtos,
-      valor_total
-    }
+      valor_total,
+    };
 
-    const html = await htmlCompiler('./src/templates/email.html', {
+    const html = await htmlCompiler("./src/templates/email.html", {
       client: existingClient.nome,
       order: newOrder[0].id,
       products: pedido_produtos.map((product) => {
         return `
           Produto #${product.produto_id} \n
           Quantidade: ${product.quantidade_produto}
-        `
+        `;
       }),
-      totalPrice: valor_total
+      totalPrice: valor_total,
     });
 
     sendEmail(existingClient.nome, existingClient.email, html);
 
     return res
       .status(201)
-      .json({ mensagem: 'Pedido realizado com sucesso', pedido: orderObject })
-
+      .json({ mensagem: "Pedido realizado com sucesso", pedido: orderObject });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ mensagem: "Erro interno do servidor" });
